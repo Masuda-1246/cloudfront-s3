c308f9cea64d4f84b11692a9af46f64dccd12cec
Masuda-1246 <yaojiezengtian38@gmail.com>
2025-07-14 12:43:46 +0900

    IP制限＋Basic認証

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -4,6 +4,7 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,27 +13,26 @@ export class InfraStack extends cdk.Stack {
     const destinationBucket = new s3.Bucket(this, 'WebsiteBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     });
 
-    const basicAuthFunction = new cloudfront.Function(
-      this,
-      "BasicAuthFunction",
-      {
-        functionName: `basic-authentication`,
-        code: cloudfront.FunctionCode.fromFile({
-          filePath: "./lib/functions/basic-auth.js",
-        }),
-      }
-    );
+    // IP制限 + Basic認証用のLambda Edge関数（us-east-1に自動デプロイ）
+    const ipRestrictionFunction = new cloudfront.experimental.EdgeFunction(this, 'IpRestrictionAndAuthFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'ip-restriction.handler',
+      code: lambda.Code.fromAsset('./lib/functions'),
+      functionName: 'ip-restriction-and-auth-lambda-edge',
+    });
 
     const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
       defaultRootObject: 'index.html',
       defaultBehavior: {
         origin: origins.S3BucketOrigin.withOriginAccessControl(destinationBucket),
-        functionAssociations: [
+        edgeLambdas: [
           {
-            function: basicAuthFunction,
-            eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
+            functionVersion: ipRestrictionFunction.currentVersion,
+            eventType: cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
           },
         ],
       }
